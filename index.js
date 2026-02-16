3443337d855ac018b081d6bbadab08a836e3d395
krishna-dev11 <krishnagothwal0@gmail.com>
2026-02-16 13:45:14 +0530

    Ready to deploy

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        // origin:"https://the-boldvoice.vercel.app",   
-        origin:"http://localhost:5173",
+        origin:"https://shree-ji-darshan-ujjain.vercel.app",   
+        // origin:"http://localhost:5173",
         credentials : true
     })
 )
